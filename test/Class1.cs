08af42edd392fd9e9b1b537c8d15aaab38333dd1
Prem Kumar.P <prem.kumar.ponuthorai@exact.com>
2018-05-04 16:52:29 +0200

    initial commit

---
+++ test/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstname, string lastname)
+        {
+            _firstName = firstname;
+            _lastName = lastname;
+        }
+
+        public string GetFullName(string _firstName, string _lastName)
+        {
+            return String.Concat(_firstName, _lastName);
+        }
+    }
+}
